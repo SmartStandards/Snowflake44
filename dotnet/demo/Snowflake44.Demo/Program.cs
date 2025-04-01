c6e92bbc0a733346ff12d5eff077094005993cc7
Vodicka, Mark <Mark.Vodicka@wtwco.com>
2025-04-01 15:11:40 +0200

    workaround for build error error CS0103

--- dotnet/demo/Snowflake44.Demo/Program.cs
+++ dotnet/demo/Snowflake44.Demo/Program.cs
@@ -7,7 +7,18 @@ namespace SmartStandards {
 
     [STAThread]
     static void Main() {
-      ApplicationConfiguration.Initialize();
+
+      // ApplicationConfiguration.Initialize(); // generates build error
+      // "error CS0103: The name 'ApplicationConfiguration' does not exist in the current context."
+
+      // workaround...
+
+      global::System.Windows.Forms.Application.EnableVisualStyles();
+      global::System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+      global::System.Windows.Forms.Application.SetHighDpiMode(HighDpiMode.SystemAware);
+
+      // ...workaround
+
       Application.Run(new MainForm());
     }
   }
